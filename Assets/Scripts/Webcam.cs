e19264f2691874b88dbf80d5535313ec0a78d44d
Charlie <charlieryancurry@gmail.com>
2025-01-21 10:33:45 -0400

    Update Webcam.cs

--- Assets/Scripts/Webcam.cs
+++ Assets/Scripts/Webcam.cs
@@ -6,7 +6,7 @@ using UnityEngine.Android;
 public class Webcam : MonoBehaviour
 {
     WebCamTexture webcamTexture;
-    Renderer renderer;
+    new Renderer renderer;
 
     // Start is called before the first frame update
     void Start()
